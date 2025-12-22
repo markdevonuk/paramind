2a82689f31771cf3dfa7a4bdd44c4107c1c248a4
markdevonuk <markdevon@gmail.com>
2025-12-22 12:08:12 +0000

    Update scenarios.js

--- js/scenarios.js
+++ js/scenarios.js
@@ -1482,7 +1482,7 @@ In DEBRIEF:
 - Give structured feedback:
 1. The Condition we were looking for was ${p.condition}.
 2. Confirm if their diagnosis matches ${p.condition}. If it does, confirm this and make a big thing of it, use words like Congratulations, You smashed it, Perfecto! , if not you can use language like "You missed the correct working impression this time but let's look closer"
-3. What they did well (2–4 bullets)
+3. What they did well (1–6 bullets) (only state a good point if the user actually did that assessment or gathered the information by questioning)
 4. Key missed questions/exams (2–4 bullets)
 5. Red flags they should have checked (from ${p.redFlags})
 6. What would be the immediate priorities prehospital (brief, UK paramedic appropriate)
