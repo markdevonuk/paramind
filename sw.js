8697273b72d594930675b7e754c14210a5b54754
Mark Devon <mark@paramind.co.uk>
2026-04-05 17:39:04 +0000

    Bump service worker to v9 to force cache refresh

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    This file enables offline functionality and caching
 */
 
-const CACHE_NAME = 'paramind-v8';
+const CACHE_NAME = 'paramind-v9';
 
 // Files to cache for offline use
 // Add your main pages and essential assets here
